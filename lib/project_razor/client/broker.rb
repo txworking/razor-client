43b199d47fd7cd36e1745bc794ed26467fb19eb9
txworking <txworking@gmail.com>
2012-11-22 22:26:49 +0800

    all broker func were done

--- lib/project_razor/client/broker.rb
+++ lib/project_razor/client/broker.rb
@@ -9,36 +9,37 @@ module ProjectRazor
 			BROKER_URL_PREFIX = ProjectRazor::DEFAULT_TARGET + ProjectRazor::BROKER_PATH
 
 			def get_all_brokers
-				
+				get(BROKER_URL_PREFIX)
 			end
 
-			def get_broker_by_uuid
-				
+			def get_broker_by_uuid(borker_uuid)
+				get("#{BROKER_URL_PREFIX}/#{broker_uuid}")
 			end
 
 			def get_broker_plugins
-				
+				get("#{BROKER_URL_PREFIX}/#{ProjectRazor::PLUGINS}")				
 			end
 
-			def add_broker
-				
+			def add_broker(options)
+				json_hash = url_encode(options)
+				logger.debug "add broker:#{BROKER_URL_PREFIX}?json_hash=#{json_hash}"
+				post("#{BROKER_URL_PREFIX}?json_hash=#{json_hash}"
 			end
 
-			def update_broker
-
+			def update_broker(borker_uuid, options)
+				json_hash = url_encode(options)
+				logger.debug "update broker:#{BROKER_URL_PREFIX}?json_hash=#{json_hash}"
+				put("#{BROKER_URL_PREFIX}/#{broker_uuid}?json_hash=#{json_hash}"
 			end
 
 			def remove_all_brokers
-				
+				delete("#{BROKER_URL_PREFIX}/#{ProjectRazor::ALL}")
 			end
 
-			def remove_broker_by_uuid
-				
+			def remove_broker_by_uuid(broker_uuid)
+				delete("#{MODEL_URL_PREFIX}/#{borker_uuid}")
 			end
 
-			def remove_broker
-				
-			end
 		end
 	end
 end
